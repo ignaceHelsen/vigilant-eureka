a69345560e9ad9b4349dda511c6c5d73dcea992f
Ignace Helsen <helsen.ignace@outlook.com>
2022-03-23 17:50:07 +0100

    First addition of hash code

--- naming-server/src/main/java/be/uantwerpen/fti/namingserver/service/HashService.java
+++ naming-server/src/main/java/be/uantwerpen/fti/namingserver/service/HashService.java
@@ -3,4 +3,11 @@ package be.uantwerpen.fti.namingserver.service;
 public class HashService {
     private String ipAddresses[];
 
+    public String calculateHash(String filename) {
+        short hashcode = (short) filename.hashCode();
+
+        // e.g. 32104
+
+
+    }
 }
