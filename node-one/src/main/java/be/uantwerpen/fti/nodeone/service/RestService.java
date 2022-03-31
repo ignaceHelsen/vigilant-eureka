55e8b4c47a0aa515f0c9ff8941de278bdd09b815
Ignace Helsen <helsen.ignace@outlook.com>
2022-03-31 16:55:42 +0200

    Addition of dns names

--- node-one/src/main/java/be/uantwerpen/fti/nodeone/service/RestService.java
+++ node-one/src/main/java/be/uantwerpen/fti/nodeone/service/RestService.java
@@ -1,25 +1,23 @@
 package be.uantwerpen.fti.nodeone.service;
 
+import be.uantwerpen.fti.nodeone.config.NamingServerConfig;
 import be.uantwerpen.fti.nodeone.domain.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class RestService {
-
     private final RestTemplate restTemplate;
+    private final NamingServerConfig namingServerConfig;
 
     public String requestNodeId(String filename) {
-        ResponseEntity<String> response = restTemplate.getForEntity(String.format("http://localhost:8080/api/naming/getIp/%s", filename), String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity(String.format("http://%s:%s/api/naming/getIp/%s",
+                namingServerConfig.getAddress(), namingServerConfig.getPort(), filename), String.class);
 
         //Handle status code
 
@@ -27,8 +25,8 @@ public class RestService {
     }
 
     public void registerNode(RegisterRequest registerRequest) {
-
-        ResponseEntity<Void> response = restTemplate.postForEntity("http://localhost:8080/api/naming/registerNode/", registerRequest, Void.class);
+        ResponseEntity<Void> response = restTemplate.postForEntity(String.format("http://%s:%s/api/naming/registerNode/",
+                namingServerConfig.getAddress(), namingServerConfig.getPort()), registerRequest, Void.class);
 
         //Handle status code
     }
