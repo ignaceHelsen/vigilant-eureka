bdd22c4fb113fcee143c4a9cf43619d3b9a40b75
BuildTools <unconfigured@null.spigotmc.org>
2022-04-07 15:45:54 +0200

    First version of bootstrap

--- node-one/src/main/java/be/uantwerpen/fti/nodeone/NodeOneApplication.java
+++ node-one/src/main/java/be/uantwerpen/fti/nodeone/NodeOneApplication.java
@@ -2,11 +2,6 @@ package be.uantwerpen.fti.nodeone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NodeOneApplication {
--- node-one/src/main/java/be/uantwerpen/fti/nodeone/service/NetworkService.java
+++ node-one/src/main/java/be/uantwerpen/fti/nodeone/service/NetworkService.java
@@ -1,54 +1,75 @@
 package be.uantwerpen.fti.nodeone.service;
 
 import be.uantwerpen.fti.nodeone.config.NetworkConfig;
+import be.uantwerpen.fti.nodeone.controller.dto.NodeStructureDto;
+import be.uantwerpen.fti.nodeone.domain.NodeStructure;
 import be.uantwerpen.fti.nodeone.domain.RegisterNodeRequest;
 import be.uantwerpen.fti.nodeone.domain.RemoveNodeRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.*;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class NetworkService implements ApplicationListener<ContextRefreshedEvent> {
-
     private final NetworkConfig networkConfig;
     private final RestService restService;
+    private final NodeStructure nodeStructure;
+    private final MulticastListener listener;
 
     public void registerNode() {
-        RegisterNodeRequest registerRequest = new RegisterNodeRequest(networkConfig.getIpAddress(), networkConfig.getHostName());
-        boolean success = restService.registerNode(registerRequest);
+        // multicast to group
+        DatagramSocket socket;
+        InetAddress group;
 
-        if (success) {
-            log.info("Registering node was successful");
+        try {
+            socket = new DatagramSocket();
+            group = InetAddress.getByName("230.0.0.0");
+            byte[] buffer = networkConfig.getHostName().getBytes();
 
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, 4446);
+            socket.send(packet);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        else {
-            log.warn("Registering node failed");
-            // Handle failure
-        }
 
+        // now listen to tcp answers in controller
+    }
+
+    private void listenForMulticast() {
+        listener.setNodes(nodeStructure);
     }
 
     public void removeNode() {
-        RemoveNodeRequest removeNodeRequest =  new RemoveNodeRequest(networkConfig.getHostName());
-            boolean success = restService.removeNode(removeNodeRequest);
+        RemoveNodeRequest removeNodeRequest = new RemoveNodeRequest(networkConfig.getHostName());
+        boolean success = restService.removeNode(removeNodeRequest);
 
         if (success) {
-                log.info("Removing node was successful");
-        }
-        else {
+            log.info("Removing node was successful");
+        } else {
             log.warn("Removing node failed");
             // Handle failure
         }
-
     }
 
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void onApplicationEvent(ContextRefreshedEvent event) { // TODO replace by ApplicationReadyEvent ?
+        // https://stackoverflow.com/questions/20275952/java-listen-to-contextrefreshedevent
         registerNode();
+        listenForMulticast();
+    }
+
+    public void setNodeStructure(NodeStructureDto structure) {
+        this.nodeStructure.setNextNode(structure.getNextNode());
+        this.nodeStructure.setPreviousNode(structure.getPreviousNode());
     }
 }
