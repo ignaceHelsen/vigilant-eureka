e75afd6cad43fe05e180274468389703e9215d9a
EliasMalomgre <elias.malomgre@gmail.com>
2022-05-04 12:43:00 +0200

    Initial commit

---
+++ node-one/src/main/java/be/uantwerpen/fti/nodeone/service/ReplicationService.java
@@ -0,0 +1,21 @@
+package be.uantwerpen.fti.nodeone.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReplicationService {
+
+    public void Shutdown() {
+        // Find previous node -> Naming server
+        // Edge case -> Preivous previous
+
+        // Send all replicated files
+        // Except not downloaded files
+
+
+        // Send log file
+
+        // Send owner of own locals files warning
+
+    }
+}
